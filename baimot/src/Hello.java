b5b1ee193f579eaba27903dbf238eac70e3c155f
Nguyen Bach <nguyendaobach@gmail.com>
2023-09-20 10:40:49 +0700

    new project

--- baimot/src/Hello.java
+++ baimot/src/Hello.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Bach");
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("What your name");
+        String name = scanner.nextLine();
+        System.out.println("Your name is "+name);
+
+
     }
 
 }
